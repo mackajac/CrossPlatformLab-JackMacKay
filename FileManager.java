d73bb90418cc81779ea80c63523594a360f118ec
mackajac <mackajac@sheridancollege.ca>
2025-10-15 14:46:00 -0400

    Fixed filemanager.java

--- FileManager.java
+++ FileManager.java
@@ -1,6 +1,5 @@
 public class FileManager {
     public static void main(String[] args) {
         System.out.println("FileManager Initialized");
-        // small change to trigger workflow
     }
 }
